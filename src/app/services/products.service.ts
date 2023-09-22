035f95361d2418ac472f3f01656a434c80cd67ee
Hellen R <roxely_garcia@hotmail.com>
2023-09-21 19:08:53 -0600

    CORS y ambientes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http'; '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
+
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://api.escuelajs.co/api/v1/products'
+  private apiUrl = `${environment.API_URL}/api/v1/products`;
 
   constructor( private http: HttpClient) {}
 
@@ -18,7 +21,8 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-      return this.http.get<Product[]>(this.apiUrl, { params });
+      return this.http.get<Product[]>(this.apiUrl, { params })
+      .pipe(retry(3));
     }
 
   getProduct(id: string){

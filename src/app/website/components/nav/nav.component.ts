43a4deb62f952a23a7a6208f3b99da9a90e670dc
Hellen R <roxely_garcia@hotmail.com>
2023-10-02 13:05:01 -0600

    save

--- src/app/website/components/nav/nav.component.ts
+++ src/app/website/components/nav/nav.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service';
-import { AuthService } from '../../services/auth.service';
-import { CategoriesService } from '../../services/categories.service';
-import { User } from '../../models/user.model';
-import { Category } from '../../models/product.model';
+import { StoreService } from '../../../services/store.service';
+import { AuthService } from '../../../services/auth.service';
+import { CategoriesService } from '../../../services/categories.service';
+import { User } from '../../../models/user.model';
+import { Category } from '../../../models/product.model';
 
 
 @Component({

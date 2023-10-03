7bdebbcaffc37495554e308b05370eaf40a59231
Hellen R <roxely_garcia@hotmail.com>
2023-10-02 19:01:21 -0600

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./website/website.module').then((m) => m.WebsiteModule),
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+  },
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
   },
   {
     path: '**',
@@ -16,6 +20,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CategoryComponent } from './pages/category/category.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { MyCartComponent } from './pages/my-cart/my-cart.component';
-import { NotFoundComponent } from '../not-found/not-found.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { RecoveryComponent } from './pages/recovery/recovery.component';
-import { RegisterComponent } from './pages/register/register.component';
+
 import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
 
 const routes: Routes = [
   {
@@ -19,19 +20,19 @@ const routes: Routes = [
       {
         path: '',
         redirectTo: '/home',
-        pathMatch: 'full',
+        pathMatch: 'full'
       },
       {
         path: 'home',
-        component: HomeComponent,
+        component: HomeComponent
       },
       {
         path: 'category/:id',
-        component: CategoryComponent,
+        component: CategoryComponent
       },
       {
         path: 'product/:id',
-        component: ProductDetailComponent,
+        component: ProductDetailComponent
       },
       {
         path: 'my-cart',
@@ -53,16 +54,14 @@ const routes: Routes = [
         path: 'profile',
         component: ProfileComponent,
       },
-      {
-        path: '**',
-        component: NotFoundComponent,
-      },
-    ],
+    ]
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class WebsiteRoutingModule {}
+export class WebsiteRoutingModule { }
+
+
